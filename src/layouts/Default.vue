53aaf2e42f90794cde6320ccf9fac45e42766705
Ricardo Morin <morinricardo@gmail.com>
2020-05-12 14:16:43 -0700

    Customize

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -22,12 +22,6 @@ query {
 </static-query>
 
 <style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
 
 .layout {
   max-width: 760px;
