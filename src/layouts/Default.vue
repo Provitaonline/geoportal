ef0269377cbb126475d39f183ae49cca56acc6fc
Ricardo Morin <morinricardo@gmail.com>
2020-06-08 16:11:45 -0700

    Main page elements

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,39 +1,39 @@
 <template>
   <div>
-    <b-navbar class="is-fixed-top">
+    <b-navbar class="is-fixed-top container is-widescreen">
         <template slot="brand">
-            <b-navbar-item>
-              <g-link :to="$tp('/')">LOGO</g-link>
-            </b-navbar-item>
+          <b-navbar-item>
+            <g-link :to="$tp('/')">LOGO</g-link>
+          </b-navbar-item>
         </template>
         <template slot="end">
-            <b-navbar-item tag="div">
-              <g-link :to="$tp('/about')">
-                {{ $t('label.about') }}
-              </g-link>
-            </b-navbar-item>
-            <b-navbar-item tag="div">
-              <g-link :to="$tp('/contact')">
-                {{ $t('label.contact') }}
-              </g-link>
-            </b-navbar-item>
-            <b-navbar-dropdown arrowless right>
-              <template slot="label">
-                {{ $t('flag') }}
-              </template>
-              <ClientOnly>
-                <b-navbar-item v-for="locale in availableLocales" :key="locale">
-                  <a @click="changeLocale(locale)">
-                    {{ $t('flag', locale) }}&nbsp&nbsp&nbsp{{ $t('language', locale) }}
-                  </a>
-                </b-navbar-item>
-              </ClientOnly>
-            </b-navbar-dropdown>
-            <b-navbar-item v-if="isAdminPage" tag="div">
-              <a @click="userLogin" class="button is-light">
-                  <font-awesome :icon="['far', 'user']"/>
-              </a>
-            </b-navbar-item>
+          <b-navbar-item tag="div">
+            <g-link :to="$tp('/about')">
+              {{ $t('label.about') }}
+            </g-link>
+          </b-navbar-item>
+          <b-navbar-item tag="div">
+            <g-link :to="$tp('/contact')">
+              {{ $t('label.contact') }}
+            </g-link>
+          </b-navbar-item>
+          <b-navbar-dropdown arrowless right>
+            <template slot="label">
+              {{ $t('flag') }}
+            </template>
+            <ClientOnly>
+              <b-navbar-item v-for="locale in availableLocales" :key="locale">
+                <a @click="changeLocale(locale)">
+                  {{ $t('flag', locale) }}&nbsp&nbsp&nbsp{{ $t('language', locale) }}
+                </a>
+              </b-navbar-item>
+            </ClientOnly>
+          </b-navbar-dropdown>
+          <b-navbar-item v-if="isAdminPage" tag="div">
+            <a @click="userLogin" class="button is-light">
+                <font-awesome :icon="['far', 'user']"/>
+            </a>
+          </b-navbar-item>
         </template>
     </b-navbar>
     <section class="hero">
@@ -78,7 +78,7 @@ query {
 
 <style lang="scss">
   .layout {
-    max-width: 760px;
+    max-width: 900px;
     margin: 0 auto;
     padding-left: 20px;
     padding-right: 20px;
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -6,36 +6,76 @@
       </h1>
     </template>
 
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
+    <b-tabs type="is-boxed" :animated="false">
+      <b-tab-item label="Archivos">
+        <g-image alt="Example image" src="~/favicon.png" width="135" />
 
-    <h1>{{ $i18n.locale }} {{ $t('message.hello') }}, world!</h1>
+        <h1>{{ $i18n.locale }} {{ $t('message.hello') }}, world!</h1>
 
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
+        </p>
 
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
+        <p class="home-links">
+          <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
+          <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
+        </p>
+        <div class="tile is-ancestor">
+          <div class="tile is-parent">
+            <div class="tile is-4 is-child box">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image is-4by3">
+                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                  </figure>
+                </div>
+                <div class="card-content">
+                  <div class="media">
+                    <div class="media-content">
+                      <p class="title is-4">Vialidad</p>
+                    </div>
+                  </div>
+                  <div class="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Phasellus nec iaculis mauris.
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="tile is-4 is-child box">
+              <p class="title">Two</p>
+            </div>
+            <div class="tile is-4 is-child box">
+              <p class="title">Three</p>
+            </div>
+          </div>
+        </div>
+      </b-tab-item>
+      <b-tab-item label="Mapa">
+        PRONTO...
+      </b-tab-item>
+    </b-tabs>
 
   </Layout>
 </template>
 
-<script>
+<style lang="scss" scoped>
 
-export default {
-  metaInfo() {
-    return {
-      title: this.$t('label.main')
-    }
-  },
-}
-</script>
-
-<style>
   .home-links a {
     margin-right: 1rem;
   }
+
+  .box {
+    box-shadow: none;
+  }
 </style>
+
+<script>
+  export default {
+    metaInfo() {
+      return {
+        title: this.$t('label.main')
+      }
+    },
+  }
+</script>
