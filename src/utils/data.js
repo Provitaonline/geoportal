54754d952206bf919256230b340ba636fb7cd218
Ricardo Morin <morinricardo@gmail.com>
2020-06-09 12:39:39 -0700

    Switch to json

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,14 +1,15 @@
 import GitHub from 'github-api'
 import axios from 'axios'
-import yaml from 'js-yaml'
 
 export async function getMetaEntries() {
   let result = {}
-  let response = await axios.get('https://raw.githubusercontent.com/jimmyangel/geoportal-data/master/meta.yaml')
+  let response = await axios.get('https://raw.githubusercontent.com/jimmyangel/geoportal-data/master/meta.json')
+  return response.data
+  /*console.log(response)
   try {
-    result = yaml.safeLoad(response.data)
+    result = JSON.parse(response.data)
   } catch (e) {
     console.error(e.name, e.message)
   }
-  return result
+  return result*/
 }
