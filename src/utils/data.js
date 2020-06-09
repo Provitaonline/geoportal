8967c425d41e7d9bb0213de359c583c95f4c0a95
Ricardo Morin <morinricardo@gmail.com>
2020-06-09 12:39:59 -0700

    Tidy up

--- src/utils/data.js
+++ src/utils/data.js
@@ -5,11 +5,4 @@ export async function getMetaEntries() {
   let result = {}
   let response = await axios.get('https://raw.githubusercontent.com/jimmyangel/geoportal-data/master/meta.json')
   return response.data
-  /*console.log(response)
-  try {
-    result = JSON.parse(response.data)
-  } catch (e) {
-    console.error(e.name, e.message)
-  }
-  return result*/
 }
