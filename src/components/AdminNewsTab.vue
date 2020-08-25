5bad476a459f4c6b7ed40bceec32abc2fc41c4b8
Ricardo Morin <morinricardo@gmail.com>
2020-08-24 18:46:58 -0700

    Add buttons to admin news tab

--- src/components/AdminNewsTab.vue
+++ src/components/AdminNewsTab.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="container" style="max-width: 900px;">
-    Coming soon...
+    <div class="buttons" style="justify-content: center;">
+      <b-button @click="" style="width: 160px;"><font-awesome :icon="['fas', 'trash-alt']"/>&nbsp;{{$t('label.removechecked')}}</b-button>
+      <b-button @click="" style="width: 160px;"><font-awesome :icon="['fas', 'plus']"/>&nbsp;{{$t('label.addnews')}}</b-button>
+    </div>
+
+  </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'AdminNewsTab'
+    name: 'AdminNewsTab',
+    data() {
+      return {
+        listOfNewsItems: [],
+        newsItemListCheckedRows: []
+      }
+    },
+    mounted() {
+      console.log('component mounted')
+    }
   }
 </script>
