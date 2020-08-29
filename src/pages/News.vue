fbd2197953222328ff4749533993bfc0ca9b6474
Ricardo Morin <morinricardo@gmail.com>
2020-08-28 18:11:32 -0700

    News page

---
+++ src/pages/News.vue
@@ -0,0 +1,91 @@
+<template>
+  <Layout>
+    <template slot="banner">
+      <h1 class="title is-uppercase">
+        {{ $t('label.news') }}
+      </h1>
+    </template>
+    <br>
+    <div class="container">
+      <div class="box">
+        <article class="media">
+          <figure class="media-left">
+            <p class="image is-270x270">
+              <img src="https://bulma.io/images/placeholders/128x128.png">
+            </p>
+          </figure>
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>Titular</strong>
+                <br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+              </p>
+            </div>
+            <nav class="level is-mobile">
+              <div class="level-left">
+                <a class="level-item">
+                  <span class="icon is-small"><i class="fas fa-reply"></i></span>
+                </a>
+                <a class="level-item">
+                  <span class="icon is-small"><i class="fas fa-retweet"></i></span>
+                </a>
+                <a class="level-item">
+                  <span class="icon is-small"><i class="fas fa-heart"></i></span>
+                </a>
+              </div>
+            </nav>
+          </div>
+        </article>
+        <article class="media">
+          <figure class="media-left">
+            <p class="image is-128x128">
+              <img src="https://bulma.io/images/placeholders/128x128.png">
+            </p>
+          </figure>
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong>John Smith</strong> <small>@johnsmith</small> <small>31m</small>
+                <br>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+              </p>
+            </div>
+            <nav class="level is-mobile">
+              <div class="level-left">
+                <a class="level-item">
+                  <span class="icon is-small"><i class="fas fa-reply"></i></span>
+                </a>
+                <a class="level-item">
+                  <span class="icon is-small"><i class="fas fa-retweet"></i></span>
+                </a>
+                <a class="level-item">
+                  <span class="icon is-small"><i class="fas fa-heart"></i></span>
+                </a>
+              </div>
+            </nav>
+          </div>
+        </article>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<style lang="scss" scoped>
+
+  .is-270x270 {
+    height: 270px;
+    width: 270px;
+  }
+
+</style>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: this.$t('label.news')
+    }
+  }
+}
+</script>
