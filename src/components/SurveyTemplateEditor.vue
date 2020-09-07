80b91bab56347c4249e0c99ac307488fcdcfc435
Ricardo Morin <morinricardo@gmail.com>
2020-09-07 15:14:19 -0700

    Question type handling

--- src/components/SurveyTemplateEditor.vue
+++ src/components/SurveyTemplateEditor.vue
@@ -1,35 +1,44 @@
 <template>
   <div class="card">
-    <ValidationObserver v-slot="{passes, dirty, failed}">
+    <ValidationObserver v-slot="{passes, dirty, pristine, failed}">
       <div style="background-color: white; padding: 12px;" class="card-header">
         <p class="card-header-title is-size-4">
-          <span v-if="!question.fieldname">{{$t('label.newquestion')}}: </span>
-          {{question.label[$i18n.locale.substr(0, 2)]}}
+          <span v-if="isNew">{{$t('label.newquestion')}}:&nbsp;</span>
+          {{q.label[$i18n.locale.substr(0, 2)]}}
         </p>
       </div>
       <div class="card-content">
         <div class="content">
           <ValidationProvider rules="required|min:4" v-slot="{ errors, valid }">
             <b-field :label="$t('label.name')" :type="{ 'is-danger': errors[0] }" :message="errors">
-              <b-input v-model="question.fieldname"></b-input>
+              <b-input v-model="q.fieldname"></b-input>
             </b-field>
           </ValidationProvider>
           <ValidationProvider rules="required|min:4" v-slot="{ errors, valid }">
             <b-field :label="$t('label.questionspanish')" :type="{ 'is-danger': errors[0] }" :message="errors">
-              <b-input v-model="question.label.es"></b-input>
+              <b-input v-model="q.label.es"></b-input>
             </b-field>
           </ValidationProvider>
           <ValidationProvider rules="required|min:4" v-slot="{ errors, valid }">
             <b-field :label="$t('label.questionenglish')" :type="{ 'is-danger': errors[0] }" :message="errors">
-              <b-input v-model="question.label.en"></b-input>
+              <b-input v-model="q.label.en"></b-input>
             </b-field>
           </ValidationProvider>
           <ValidationProvider rules="required" v-slot="{ errors, valid }">
-            <label class="label">{{$t('label.questiontype')}}</label>
-            <div class="block">
-              <b-radio v-model="question.type" name="type" native-value="select">{{$t('label.selection')}}</b-radio>
-              <b-radio v-model="question.type" name="type" native-value="text">{{$t('label.text')}}</b-radio>
-            </div>
+            <b-field :label="$t('label.questiontype')" :type="{ 'is-danger': errors[0] }" :message="errors">
+              <b-radio v-model="q.type" name="type" native-value="text">{{$t('label.text')}}</b-radio>
+              <b-radio @input="selectEntry()" v-model="q.type" name="type" native-value="select">{{$t('label.selection')}}</b-radio>
+            </b-field>
+          </ValidationProvider>
+          <ValidationProvider rules="required" v-slot="{ errors, valid }">
+            <b-field v-if="q.type === 'select'" :label="$t('label.optionsspanish')" :type="{ 'is-danger': errors[0] }" :message="errors">
+              <b-taginput v-model="q.options.es" :placeholder="$t('label.addoption')"></b-taginput>
+            </b-field>
+          </ValidationProvider>
+          <ValidationProvider rules="required" v-slot="{ errors, valid }">
+            <b-field v-if="q.type === 'select'" :label="$t('label.optionsenglish')" :type="{ 'is-danger': errors[0] }" :message="errors">
+              <b-taginput v-model="q.options.en" :placeholder="$t('label.addoption')"></b-taginput>
+            </b-field>
           </ValidationProvider>
         </div>
       </div>
@@ -37,7 +46,7 @@
       <div class="card-footer" style="padding: 24px; justify-content: flex-end;">
         <div class="buttons">
           <b-button @click="$parent.close()" style="width: 140px;"><font-awesome :icon="['fas', 'times']"/>&nbsp;{{$t('label.cancel')}}</b-button>
-          <b-button @click="passes(acceptChanges)" :disabled="failed || !dirty" style="width: 140px;"><font-awesome :icon="['fas', 'check']"/>&nbsp;{{$t('label.save')}}</b-button>
+          <b-button @click="passes(acceptChanges)" :disabled="failed || pristine" style="width: 140px;"><font-awesome :icon="['fas', 'check']"/>&nbsp;{{$t('label.save')}}</b-button>
         </div>
       </div>
 
@@ -66,7 +75,8 @@ export default {
   },
   data() {
     return {
-
+      q: this.question,
+      isNew: false
     }
   },
   components: {
@@ -76,10 +86,16 @@ export default {
   beforeCreate() {
     validation.localize(this.$i18n.locale.toString().substr(0,2))
   },
+  created() {
+    if (!this.question.fieldname) this.isNew = true
+  },
   methods: {
     acceptChanges() {
-      this.$eventBus.$emit('acceptquestionchanges', this.question)
+      this.$eventBus.$emit('acceptquestionchanges', this.q)
       this.$parent.close()
+    },
+    selectEntry() {
+      if (!this.q.options) this.q.options = {en: [], es: []}
     }
   }
 }
