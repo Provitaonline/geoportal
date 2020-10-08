945f6699d6746ce52b0cb5ffc50ec97a5f28e236
Ricardo Morin <morinricardo@gmail.com>
2020-10-07 17:33:09 -0700

    Tweak

--- src/layouts/NewsLayout.vue
+++ src/layouts/NewsLayout.vue
@@ -6,9 +6,11 @@
         <div v-if="yyyymm">
           ({{ getMonthName(yyyymm) + ' ' + yyyymm.substr(0, 4) }})
         </div>
-        <a v-else title="RSS feed" :href="'/rss-' + $i18n.locale.substr(0, 2) + '.xml'">
-          <div class="is-size-5 rss-icon"><font-awesome :icon="['fas', 'rss']"/></div>
-        </a>
+        <div v-else>
+          <a title="RSS feed" :href="'/rss-' + $i18n.locale.substr(0, 2) + '.xml'">
+            &nbsp;<span class="is-size-5 rss-icon"><font-awesome :icon="['fas', 'rss']"/></span>
+          </a>
+        </div>
       </div>
     </template>
     <br>
