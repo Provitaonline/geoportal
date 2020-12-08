ffacaf25b100aba6ceb9a992c9a5264cb64b5c82
Ricardo Morin <morinricardo@gmail.com>
2020-12-08 15:27:12 -0800

    Pattern isFlipped

--- src/utils/misc.js
+++ src/utils/misc.js
@@ -26,13 +26,17 @@ export function makeColorTableParameter(colorTable) {
   return ctp
 }
 
-export function stripePattern(color) {
+export function stripePattern(color, isFlipped) {
   return new Promise((resolve, reject) => {
     let canvas = document.createElement('canvas')
     let ctx = canvas.getContext('2d')
     let img = new Image()
     img.src = '/images/stripes.svg'
     img.onload = () => {
+      if (isFlipped) {
+        ctx.translate(10, 0)
+        ctx.scale(-1, 1)
+      }
       ctx.drawImage(img, 0, 0)
       ctx.globalCompositeOperation = 'source-in'
       ctx.fillStyle = color
