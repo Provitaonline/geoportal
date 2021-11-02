c66a13178d774b08a92d4b517049bc35bf855528
Ricardo Morin <morinricardo@gmail.com>
2021-11-02 13:17:10 -0700

    Update common meta component

--- src/components/admin/CommonMeta.vue
+++ src/components/admin/CommonMeta.vue
@@ -10,6 +10,43 @@
         <b-input v-model="commonMetaFlat['name.en']"></b-input>
       </b-field>
     </ValidationProvider>
+    <div class="columns">
+      <div class="column">
+        <ValidationProvider rules="required|min:4" v-slot="{ errors, valid }">
+          <b-field :label="$t('label.source')" :type="{ 'is-danger': errors[0] }" :message="errors">
+            <b-input v-model="commonMetaFlat['source']"></b-input>
+          </b-field>
+        </ValidationProvider>
+      </div>
+      <div class="column">
+        <ValidationProvider rules="required" v-slot="{ errors, valid }">
+          <b-field :label="$t('label.date')" :type="{ 'is-danger': errors[0] }" :message="errors">
+            <b-datepicker v-model="formDate" :locale="$i18n.locale" icon="calendar" trap-focus></b-datepicker>
+          </b-field>
+        </ValidationProvider>
+      </div>
+    </div>
+    <ValidationProvider rules="required" v-slot="{ errors, valid }">
+      <b-field :label="$t('label.tagsspanish')" :type="{ 'is-danger': errors[0] }" :message="errors">
+        <b-taginput v-model="esTags" :placeholder="$t('label.addtag')"></b-taginput>
+      </b-field>
+    </ValidationProvider>
+    <ValidationProvider rules="required" v-slot="{ errors, valid }">
+      <b-field :label="$t('label.tagsenglish')" :type="{ 'is-danger': errors[0] }" :message="errors">
+        <b-taginput v-model="enTags" :placeholder="$t('label.addtag')"></b-taginput>
+      </b-field>
+    </ValidationProvider>
+    <ValidationProvider rules="required|min:4" v-slot="{ errors, valid }">
+      <b-field :label="$t('label.descriptionspanish') + ' (markdown)'" :type="{ 'is-danger': errors[0] }" :message="errors">
+        <b-input v-model="commonMetaFlat['description.es']" type="textarea"></b-input>
+      </b-field>
+    </ValidationProvider>
+    <ValidationProvider rules="required|min:4" v-slot="{ errors, valid }">
+      <b-field :label="$t('label.descriptionenglish') + ' (markdown)'" :type="{ 'is-danger': errors[0] }" :message="errors">
+        <b-input v-model="commonMetaFlat['description.en']" type="textarea"></b-input>
+      </b-field>
+    </ValidationProvider>
+    <br>
   </div>
 </template>
 
@@ -17,14 +54,73 @@
 import { ValidationObserver, ValidationProvider } from 'vee-validate'
 import * as validation from '~/utils/validation'
 
+let flatten = require('flat')
+let unflatten = require('flat').unflatten
+
 export default {
   name: 'CommonMeta',
   props: {
     commonMetaFlat: { type: Object, required: true }
   },
+  data() {
+    return {
+      //formDate: null
+    }
+  },
   components: {
     ValidationObserver,
     ValidationProvider
+  },
+  beforeCreate() {
+    validation.localize(this.$i18n.locale.toString().substr(0,2))
+  },
+  methods: {
+    unflattenTags(lang) {
+      let kwds = []
+      let i = 0
+      while (this.commonMetaFlat['keywords.' + lang + '.' + i]) {
+        kwds.push(this.commonMetaFlat['keywords.' + lang + '.' + i++])
+      }
+      return kwds
+    },
+    reflattenTags(lang, val) {
+      let obj = {}
+      obj['keywords.' + lang] = val
+      let i = 0
+      while (this.commonMetaFlat['keywords.' + lang + '.' + i]) {
+        delete (this.commonMetaFlat['keywords.' + lang + '.' + i++])
+      }
+      let f = flatten(obj)
+      Object.keys(f).forEach((key) => {
+        this.$set(this.commonMetaFlat, key, f[key])
+      })
+    }
+  },
+  computed: {
+    esTags: {
+      get() {
+        return this.unflattenTags('es')
+      },
+      set(val) {
+        this.reflattenTags('es', val)
+      }
+    },
+    enTags: {
+      get() {
+        return this.unflattenTags('en')
+      },
+      set(val) {
+        this.reflattenTags('en', val)
+      }
+    },
+    formDate: {
+      get() {
+        return this.commonMetaFlat.date ? new Date(this.commonMetaFlat.date) : new Date()
+      },
+      set(val) {
+        this.commonMetaFlat.date = val.toISOString()
+      }
+    }
   }
 }
 </script>
